bf7070e5b43f1be81ab99e203105646409b356b0
VKalaitanov <kalaytanov93@gmail.com>
2024-05-16 11:37:25 +0200

    add settings django port

--- shopmoderno/settings.py
+++ shopmoderno/settings.py
@@ -17,8 +17,8 @@ DEBUG = int(env('DEBUG', default=1))
 
 # CART_SESSION_ID = 'cart'
 
-# ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost', 'shopmoderno.ru']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
+# ALLOWED_HOSTS = ["127.0.0.1", 'localhost', 'shopmoderno.ru']
 
 INTERNAL_IPS = ["127.0.0.1"]
 
@@ -85,27 +85,27 @@ WSGI_APPLICATION = 'shopmoderno.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB'),
-#         'USER': env('POSTGRES_USER'),
-#         'PASSWORD': env('POSTGRES_PASSWORD'),
-#         'HOST': env('POSTGRES_HOST'),
-#         'PORT': env('POSTGRES_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',#env('POSTGRES_HOST'),
-        'PORT': 5433, #env('POSTGRES_PORT'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('POSTGRES_DB'),
+#         'USER': env('POSTGRES_USER'),
+#         'PASSWORD': env('POSTGRES_PASSWORD'),
+#         'HOST': 'localhost',#env('POSTGRES_HOST'),
+#         'PORT': 5433, #env('POSTGRES_PORT'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
